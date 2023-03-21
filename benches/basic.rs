2bbfa34818091ed337c327929409da23b33ad380
Billy Chan <ccw.billy.123@gmail.com>
2023-03-21 15:58:53 +0800

    benchmarks

--- benches/basic.rs
+++ benches/basic.rs
@@ -26,11 +26,41 @@ fn select_and_to_string() {
     select().to_string(MysqlQueryBuilder);
 }
 
+const NAMES: [i32; 1_000_000] = [0; 1_000_000];
+
+fn mut_iter() {
+    let mut sql = String::new();
+    let mut names_iter = NAMES.iter();
+
+    if let Some(name) = names_iter.next() {
+        write!(sql, "{}", name).unwrap();
+    }
+
+    for name in names_iter {
+        write!(sql, ", ").unwrap();
+        write!(sql, "{}", name).unwrap();
+    }
+}
+
+fn fold() {
+    let mut sql = String::new();
+
+    NAMES.iter().fold(true, |first, name| {
+        if !first {
+            write!(sql, ", ").unwrap();
+        }
+        write!(sql, "{}", name).unwrap();
+        false
+    });
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("vanilla", |b| b.iter(vanilla));
     c.bench_function("select", |b| b.iter(select));
     c.bench_function("select_and_build", |b| b.iter(select_and_build));
     c.bench_function("select_and_to_string", |b| b.iter(select_and_to_string));
+    c.bench_function("mut_iter", |b| b.iter(mut_iter));
+    c.bench_function("fold", |b| b.iter(fold));
 }
 
 criterion_group!(benches, criterion_benchmark);
